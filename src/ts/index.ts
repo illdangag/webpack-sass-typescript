77e2aa3d560b49738eeeb6129c210d73751b1c8d
ybchoi <ybchoi@forcs.com>
2020-12-10 15:53:34 +0900

    redux

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,29 @@
 import '../css/style.scss';
+import { store, actionType, } from './store';
 
 import Car from './car/car';
-
 const car: Car = new Car();
+
+const render1 = () => {
+  console.log('[Render1]', store.getState());
+};
+
+const render2 = () => {
+  console.log('[Render2]', store.getState());
+};
+
+store.subscribe(render1);
+store.subscribe(render2);
+
+const incrementElement = document.getElementById('increment');
+if (incrementElement) {
+  incrementElement.addEventListener('click', function () {
+    store.dispatch({ type: actionType.INCREMENT });
+  });
+}
+const decrementElement = document.getElementById('decrement');
+if (decrementElement) {
+  decrementElement.addEventListener('click', function () {
+    store.dispatch({ type: actionType.DECREMENT });
+  });
+}
---
+++ src/ts/store.ts
@@ -0,0 +1,38 @@
+import { createStore, } from 'redux';
+
+type State = {
+  count: number;
+}
+
+type Action = {
+  type: actionType;
+}
+
+const initialState: State = {
+  count: 0,
+}
+
+export enum actionType {
+  INCREMENT = 'INCREMENT',
+  DECREMENT = 'DECREMENT',
+}
+
+function reducer(state: State = initialState, action: Action): State {
+  console.log(action);
+  switch (action.type) {
+    case actionType.INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case actionType.DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      return state
+  }
+}
+
+export const store = createStore(reducer);
